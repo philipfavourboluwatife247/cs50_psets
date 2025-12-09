3ccd90f865395966afb08ab6ad22c8f729146b19
Philip Favour Boluwatife <113596809+philipfavourboluwatife247@users.noreply.github.com>
2025-12-09 18:12:56 +0100

    fixed a bug in the loop

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -2,11 +2,16 @@
 Author - Philip Favour Boluwatife
 Date - 8th December, 2025
 Pset Name - Grocery List
-Pset Detail - implement a program that prompts the user for items, one per line, until the user inputs control-d (which is a common way of ending one’s input to a program). Then output the user’s grocery list in all uppercase, sorted alphabetically by item, prefixing each line with the number of times the user inputted that item. No need to pluralize the items. Treat the user’s input case-insensitively.
+Pset Detail - implement a program that prompts the user for items, one per line,
+until the user inputs control-d (which is a common way of ending one’s input to
+a program). Then output the user’s grocery list in all uppercase, sorted
+alphabetically by item, prefixing each line with the number of times the user
+inputted that item. No need to pluralize the items. Treat the user’s input
+case-insensitively.
 """
 
 def main():
-    print(grocery_list())
+    grocery_list()
 
 def grocery_list():
     groceries = {}
@@ -18,8 +23,11 @@ def grocery_list():
             else:
                 groceries[item] = 1
         except EOFError:
-            for item, count in sorted(groceries.items()):
-                return "\n".join(f"{count} {item.upper()}" for item, count in sorted(groceries.items()))
-                
+            break
+
+    for item, count in sorted(groceries.items()):
+        print(f"{count} {item.upper()}")
+
+
 if __name__ == "__main__":
     main()
